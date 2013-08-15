2811e1ecd25dd3db47984d55396436cf734b806f
Brecht <brecht357@gmail.com>
2013-08-15 15:49:26 +0200

    initial commit

---
+++ src/be/brecht/loanit/AddLoan.java
@@ -0,0 +1,13 @@
+package be.brecht.loanit;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+
+/**
+ * Controlls the form for adding new loans
+ * @author Brecht Bonte
+ */
+@TargetApi(Build.VERSION_CODES.GINGERBREAD) public class AddLoan extends Activity {
+
+}
