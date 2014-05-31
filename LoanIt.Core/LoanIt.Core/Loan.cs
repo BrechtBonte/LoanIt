c028fc88d227e548eb7dde598bfa0e5336a020b3
Brecht Bonte <nbz_tricksta@hotmail.com>
2014-05-31 12:16:20 +0200

    added core library

---
+++ LoanIt.Core/LoanIt.Core/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LoanIt.Core
+{
+	public class Loan
+	{
+		protected int amount;
+		protected bool isOwedToUser;
+		protected Person person;
+
+		public Loan(int amount, bool isOwedToUser, Person person)
+		{
+			this.amount = amount;
+			this.isOwedToUser = isOwedToUser;
+			this.person = person;
+		}
+	}
+}
